9c7efe2369cc181f4bd4e9674faa0f66f4d759dd
jesuispy <mousbrainsession@hotmail.com>
2021-08-12 21:04:57 -0400

    added the first test and github actions

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# from django.test import TestCase
+from unittest import TestCase
 
 # Create your tests here.
+
+
+def two_intergers_sum(a, b):
+    return a + b
+
+
+class TestSum(TestCase):
+    def test_sum(self):
+        self.assertEqual(two_intergers_sum(1, 2), 3)
